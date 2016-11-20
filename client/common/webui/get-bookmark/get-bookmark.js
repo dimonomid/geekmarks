2e14bd34cb3d419539496359acbd0b26053e5cf6
Dmitry Frank <mail@dmitryfrank.com>
2016-11-20 10:47:10 +0200

    Close bookmark selection window when selected

--- client/common/webui/get-bookmark/get-bookmark.js
+++ client/common/webui/get-bookmark/get-bookmark.js
@@ -38,6 +38,13 @@
                   div.find("#bkm_link").attr('href', bkm.url);
                   div.find("#bkm_link").attr('target', '_blank');
 
+                  // Just after user clicked at some bookmark, close the
+                  // bookmark selection window
+                  div.find("#bkm_link").click(function() {
+                    window.close();
+                    return true;
+                  })
+
                   div.find("#edit_link").click(function() {
                     gmPageWrapper.openPageEditBookmarks(bkm.id);
                     return false;
