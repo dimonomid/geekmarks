6c4446ea70ef7d8c558fba103dc5463623bb74aa
Dmitry Frank <mail@dmitryfrank.com>
2016-11-20 13:41:01 +0200

    Get-bookmark shows untagged bookmarks by default

--- client/common/webui/get-bookmark/get-bookmark.js
+++ client/common/webui/get-bookmark/get-bookmark.js
@@ -18,50 +18,55 @@
       },
 
       onChange: function(selectedTags) {
-        gmClient.getTaggedBookmarks(selectedTags.tagIDs, function(status, resp) {
-          console.log('resp bkm2:', status, resp)
-
-          if (status === 200) {
-            var listElem = contentElem.find("#tmpdata");
-            listElem.text("");
-
-            resp.forEach(function(bkm) {
-              var div = jQuery('<div/>', {
-                id: 'bookmark_' + bkm.id,
-                class: 'bookmark-div',
-              });
-              div.load(
-                srcDir + "/bkm.html",
-                undefined,
-                function() {
-                  div.find("#bkm_link").html(bkm.title);
-                  div.find("#bkm_link").attr('href', bkm.url);
-                  div.find("#bkm_link").attr('target', '_blank');
-
-                  // Just after user clicked at some bookmark, close the
-                  // bookmark selection window
-                  div.find("#bkm_link").click(function() {
-                    window.close();
-                    return true;
-                  })
-
-                  div.find("#edit_link").click(function() {
-                    gmPageWrapper.openPageEditBookmarks(bkm.id);
-                    return false;
-                  })
-
-                  div.appendTo(listElem);
-                  console.log('html:', div.html());
-                }
-              );
-            });
-          } else {
-            // TODO: show error
-          }
-
-        })
+        gmClient.getTaggedBookmarks(selectedTags.tagIDs, onBookmarksReceived);
       }
     });
+
+    gmClient.onConnected(true, function() {
+      gmClient.getTaggedBookmarks([], onBookmarksReceived);
+    });
+
+    function onBookmarksReceived(status, resp) {
+      console.log('resp bkm2:', status, resp)
+
+      if (status === 200) {
+        var listElem = contentElem.find("#tmpdata");
+        listElem.text("");
+
+        resp.forEach(function(bkm) {
+          var div = jQuery('<div/>', {
+            id: 'bookmark_' + bkm.id,
+            class: 'bookmark-div',
+          });
+          div.load(
+            srcDir + "/bkm.html",
+            undefined,
+            function() {
+              div.find("#bkm_link").html(bkm.title);
+              div.find("#bkm_link").attr('href', bkm.url);
+              div.find("#bkm_link").attr('target', '_blank');
+
+              // Just after user clicked at some bookmark, close the
+              // bookmark selection window
+              div.find("#bkm_link").click(function() {
+                window.close();
+                return true;
+              })
+
+              div.find("#edit_link").click(function() {
+                gmPageWrapper.openPageEditBookmarks(bkm.id);
+                return false;
+              })
+
+              div.appendTo(listElem);
+              console.log('html:', div.html());
+            }
+          );
+        });
+      } else {
+        // TODO: show error
+      }
+    }
   }
 
   exports.init = init;
