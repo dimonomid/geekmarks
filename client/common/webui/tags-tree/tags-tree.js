ffe2fa1a0965149e2f2871f3e06d0e9dc4c1b26e
Dmitry Frank <mail@dmitryfrank.com>
2016-12-10 23:25:42 +0200

    Refactor tags tree stub with the fancytree plugin

--- client/common/webui/tags-tree/tags-tree.js
+++ client/common/webui/tags-tree/tags-tree.js
@@ -6,10 +6,20 @@
 
   function init(gmClient, _contentElem, srcDir, queryParams, curTabData) {
     contentElem = _contentElem;
-    var treeTable = contentElem.find('#tree_table')
+    var tagsTreeDiv = contentElem.find('#tags_tree_div')
 
-    treeTable.treetable({
-      expandable: true,
+    tagsTreeDiv.fancytree({
+      source: [
+        {title: "Node 1", key: "1"},
+        {title: "Folder 2", key: "2", folder: true, children: [
+          {title: "Node 2.1", key: "3"},
+          {title: "Node 2.2", key: "4"}
+        ]},
+        {title: "Folder 3", key: "5", folder: true, children: [
+          {title: "Node 3.1", key: "6"},
+          {title: "Node 3.2", key: "7"}
+        ]}
+      ],
     });
   }
 
