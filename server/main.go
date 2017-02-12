c44d4c8e7aa33a41db3bda7db96b2dc499ef06b6
Dmitry Frank <mail@dmitryfrank.com>
2017-02-12 23:57:36 +0200

    Remove adding of test users

--- server/main.go
+++ server/main.go
@@ -4,14 +4,11 @@
 package main
 
 import (
-	"database/sql"
 	"flag"
 	"fmt"
 	"net/http"
 
-	"dmitryfrank.com/geekmarks/server/cptr"
 	gmserver "dmitryfrank.com/geekmarks/server/server"
-	"dmitryfrank.com/geekmarks/server/storage"
 	storagecommon "dmitryfrank.com/geekmarks/server/storage/common"
 	"github.com/golang/glog"
 	"github.com/juju/errors"
@@ -46,11 +43,6 @@ func main() {
 		glog.Fatalf("%s\n", errors.ErrorStack(err))
 	}
 
-	err = createTestUsers(si)
-	if err != nil {
-		glog.Fatalf("%s\n", errors.ErrorStack(err))
-	}
-
 	handler, err := gminstance.CreateHandler()
 	if err != nil {
 		glog.Fatalf("%s\n", errors.ErrorStack(err))
@@ -58,54 +50,4 @@ func main() {
 
 	glog.Infof("Listening at the port %s ...", *port)
 	http.ListenAndServe(fmt.Sprintf(":%s", *port), handler)
-}
-
-// TODO: remove
-func createTestUsers(si storage.Storage) error {
-	err := si.Tx(func(tx *sql.Tx) error {
-
-		_, err := si.GetUser(tx, &storage.GetUserArgs{
-			ID: cptr.Int(1),
-		})
-
-		if err != nil {
-			if errors.Cause(err) == storage.ErrUserDoesNotExist {
-				glog.Infof("Creating test user: alice")
-				_, err := si.CreateUser(tx, &storage.UserData{
-					Username: "alice",
-					Password: "alice",
-					Email:    "alice@domain.com",
-				})
-				if err != nil {
-					return errors.Trace(err)
-				}
-			} else {
-				return errors.Trace(err)
-			}
-		}
-
-		_, err = si.GetUser(tx, &storage.GetUserArgs{
-			ID: cptr.Int(2),
-		})
-
-		if err != nil {
-			if errors.Cause(err) == storage.ErrUserDoesNotExist {
-				glog.Infof("Creating test user: bob")
-				_, err := si.CreateUser(tx, &storage.UserData{
-					Username: "bob",
-					Password: "bob",
-					Email:    "bob@domain.com",
-				})
-				if err != nil {
-					return errors.Trace(err)
-				}
-			} else {
-				return errors.Trace(err)
-			}
-		}
-
-		return nil
-	})
-
-	return errors.Trace(err)
 }
