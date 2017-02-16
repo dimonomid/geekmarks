d960996442e0bf00fb303a32411475eec30a2ff0
Dmitry Frank <mail@dmitryfrank.com>
2017-02-17 01:52:11 +0200

    Add geekmarks.crx to the webroot
    It's not used right now, because Chrome finally published the extension,
    but let it be here for now

--- server/server/webroot/index.js
+++ server/server/webroot/index.js
@@ -2,16 +2,9 @@
   $(document).ready(function() {
     $(".logo-large").html("<p>" + gmLogo.getLogoDataHtml() + "</p>")
 
-    $("#get_ext").click(function() {
-      alert(""
-        + "Currently there are issues with publishing the extension on the "
-        + "Chrome Web Store. If you want to try it right now, you can still "
-        + "install the unpacked extension: clone the Geekmarks git repository, "
-        + "navigate to chrome://extensions in your Chrome, expand the developer dropdown menu "
-        + "and click \"Load Unpacked Extension\", select the folder client/chrome-ext "
-        + "from the repository."
-      );
-      return false;
-    })
+    //$("#get_ext").click(function() {
+      //$("#get_ext_msg").toggle();
+      //return false;
+    //});
   })
 })();
