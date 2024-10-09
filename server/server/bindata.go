3149298cff1fb333e71be93819c71c82f3eed2fd
Dmitry Frank <mail@dmitryfrank.com>
2024-10-09 07:30:40 +0300

    Update the Fork me on Github ribbon

--- server/server/bindata.go
+++ server/server/bindata.go
@@ -237,7 +237,7 @@ var _webrootIndexHtml = []byte(`<!DOCTYPE html>
 
   <body>
 
-    <a href="https://github.com/dimonomid/geekmarks"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://camo.githubusercontent.com/121cd7cbdc3e4855075ea8b558508b91ac463ac2/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f677265656e5f3030373230302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_green_007200.png"></a>
+    <a href="https://github.com/dimonomid/geekmarks"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://github.blog/wp-content/uploads/2008/12/forkme_left_green_007200.png" alt="Fork me on GitHub" loading="lazy"></a>
 
     <div class="header">
 
@@ -411,7 +411,7 @@ func webrootIndexHtml() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "webroot/index.html", size: 6307, mode: os.FileMode(420), modTime: time.Unix(1, 0)}
+	info := bindataFileInfo{name: "webroot/index.html", size: 6095, mode: os.FileMode(420), modTime: time.Unix(1, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
